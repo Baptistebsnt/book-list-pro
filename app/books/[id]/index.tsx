8f9fbe963e5f874da1d340f0aef4145133b1de50
Baptistebsnt <baptiste.boissonnet@gmail.com>
2026-09-10 11:58:58 +0200

    fix: remove stack.screen to bookdetails

--- app/books/[id]/index.tsx
+++ app/books/[id]/index.tsx
@@ -1,10 +1,19 @@
-import { useLocalSearchParams } from "expo-router"
+import { Stack, useLocalSearchParams } from "expo-router"
 import { BookDetail } from "@/features/books/book-detail"
+import { useBook } from "@/services/query/books"
+
+const FALLBACK_TITLE = "Fiche de l'ouvrage"
 
 const BookDetailsRoute = () => {
   const { id } = useLocalSearchParams<{ id: string }>()
+  const { data } = useBook(id)
 
-  return <BookDetail id={id} />
+  return (
+    <>
+      <Stack.Screen options={{ title: data?.titre ?? FALLBACK_TITLE }} />
+      <BookDetail id={id} />
+    </>
+  )
 }
 
 export default BookDetailsRoute
