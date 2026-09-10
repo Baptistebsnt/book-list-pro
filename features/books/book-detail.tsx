d1e654e2ba3600c9a1abe26b8e20ee4e07dd2ce2
Baptistebsnt <baptiste.boissonnet@gmail.com>
2026-09-10 11:58:58 +0200

    fix: remove stack.screen to bookdetails

--- features/books/book-detail.tsx
+++ features/books/book-detail.tsx
@@ -1,4 +1,3 @@
-import { Stack } from "expo-router"
 import { ScrollView } from "react-native"
 import { BookDetailContent } from "@/components/books/book-detail-content"
 import { useBook } from "@/services/query/books"
@@ -7,20 +6,12 @@ type BookDetailProps = {
   id: string
 }
 
-const FALLBACK_TITLE = "Fiche de l'ouvrage"
-
 export const BookDetail = ({ id }: BookDetailProps) => {
   const query = useBook(id)
 
   return (
-    <>
-      <Stack.Screen options={{ title: query.data?.titre ?? FALLBACK_TITLE }} />
-      <ScrollView
-        contentContainerClassName="gap-6 p-4"
-        className="bg-background"
-      >
-        <BookDetailContent query={query} />
-      </ScrollView>
-    </>
+    <ScrollView contentContainerClassName="gap-6 p-4" className="bg-background">
+      <BookDetailContent query={query} />
+    </ScrollView>
   )
 }
